df44707ffd31bcf651ee22b89bc24562c61921d6
Castillo Cañete <ljcastillo@indra.es>
2017-09-27 18:02:56 +0200

    http ejemplo

---
+++ 09_http/http-aula/src/app/app.module.ts
@@ -0,0 +1,25 @@
+// Módulos de Angular
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+// Módulos propios
+
+
+import { AppComponent } from './app.component';
+import { BuscarComponent } from './buscar/buscar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuscarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
