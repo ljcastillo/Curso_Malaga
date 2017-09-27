009a177e65fcb44a9205adb57f6bffb08ba4c6c9
Castillo Cañete <ljcastillo@indra.es>
2017-09-27 16:44:35 +0200

    Servicios

---
+++ 08_servicios/servicio-aula/src/app/app.module.ts
@@ -0,0 +1,24 @@
+// Módulos de Angular
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core';
+// Módulos propios
+import { ServicesModule } from './services/services.module';
+
+import { AppComponent } from './app.component';
+import { BuscarComponent } from './buscar/buscar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuscarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ServicesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 08_servicios/servicio-aula/src/app/services/libros.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LibrosService {
+
+  listaLibros: Array<string>;
+
+  constructor() { }
+
+  getLibros(libro) {
+    this.listaLibros = [
+      'Angular 4',
+      'Todo Angular',
+      'Angular para expertos'
+    ]
+    return (this.listaLibros);
+  }
+
+}
